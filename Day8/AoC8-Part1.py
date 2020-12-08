871814e70f141858862d0c54410b10c474c55970
Kenny Balding <kbalding@outlook.com>
2020-12-08 22:57:10 +0000

    Day 8 part 1

--- Day8/AoC8-Part1.py
+++ Day8/AoC8-Part1.py
@@ -1,18 +1,37 @@
-accumulator = 0
-
-with open('Day8\\test.txt') as data:
+with open('Day8\\input.txt') as data:
     data = data.readlines()
     data = [line.strip() for line in data]
 
+    
+    accumulator = 0
+    currentIndex = 0
+    checked = set()
 
 
 
-
-for i in data:
-    operator, direction = (i.split(' '))
-    print(int(direction))
-
-
+while currentIndex <= len(data):  
+    if currentIndex in checked:
+        print(accumulator)
+        #print(checked)
+        break
+    #print(checked)
+    checked.add(currentIndex)
+    operation = data[currentIndex].split()
+    if operation[0] == 'acc':
+        #print(int(direction))
+        #print(accumulator)
+        accumulator+=int(operation[1])
+        currentIndex += 1        
+    elif operation[0] == 'nop':
+        #print(int(direction))
+        currentIndex += 1       
+    elif operation[0] == 'jmp':
+        #print(int(direction))
+        currentIndex += int(operation[1])
+    else:
+        print('ERROR')
+       
+print(checked)
 
 
 
